715bf9718fd68bd88bc3ef198fbf452470e99d1e
Bal Krishna Pandey <balkrishnapandey21@gmail.com>
2024-04-01 00:43:38 +0530

    first commit

---
+++ src/EmployeeData.js
@@ -0,0 +1,30 @@
+ export const EmployeeData=[
+    {
+        id:1,
+        firstName:'Mark',
+        lastName:'David',
+        age:20
+
+    },
+    {
+        id:2,
+        firstName:'Virat',
+        lastName:'Kohli',
+        age:20
+
+    },
+    {
+        id:3,
+        firstName:'Amit',
+        lastName:'Badana',
+        age:20
+
+    },
+    {
+        id:4,
+        firstName:'Kamlesh',
+        lastName:'Tripathi',
+        age:25
+
+    },
+]
